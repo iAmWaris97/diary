d45416f886210240953887d5b3bfbbcb1e1d9645
iAmWaris97 <haleem.waris@gmail.com>
2023-01-28 19:42:12 +0430

    Set up associations between models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  after_save :update_comments_counter
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+  belongs_to :author, class_name: 'User'
+  after_save :update_posts_counter
 end
