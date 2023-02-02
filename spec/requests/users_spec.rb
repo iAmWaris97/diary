4ef6fd5a8b2181cfe9898b7dfb42fb093ed50611
iAmWaris97 <haleem.waris@gmail.com>
2023-02-02 13:09:18 +0430

    Test index action of users controller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'UsersController', type: :request do
+  subject { User.new(name: 'Waris', photo: 'Waris Profile', bio: 'Software Developer', postCounter: 2) }
+
+  before { subject.save } 
+
+  describe "GET #index" do
+    before(:example) { get users_path }
+    it "returns http success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the correct routes" do
+      expect(response).to render_template(:index)
+    end
+
+    it "has the correct placeholder text in the response body" do
+      expect(response.body).to include("This page lists all the users!")
+    end
+  end
+end
