67816c5708c074cc3f86086503c94ffa7f54a2f9
iAmWaris97 <haleem.waris@gmail.com>
2023-02-05 07:39:28 +0430

    update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  after_save :update_posts_ounter
+  after_save :update_posts_counter
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
