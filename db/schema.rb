ac39e953c0348347f2aad71165c95ebd01d5a673
iAmWaris97 <haleem.waris@gmail.com>
2023-02-06 23:25:17 +0430

    rename postsCounter column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_122130) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_184949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_122130) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "postCounter", default: 0
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
