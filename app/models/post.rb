dfb94fd88bc1cb8f6cf770d3825d6e10a1b7362c
iAmWaris97 <haleem.waris@gmail.com>
2023-02-07 18:43:20 +0430

    update user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-  after_save :update_posts_counter
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
@@ -8,6 +7,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 3..250 }
   validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_posts_counter
 
   def last_five_comments
     comments.order(created_at: :desc).limit(5)
