474741e9bf340a225c4b41a9a54fb175b50b0d44
iAmWaris97 <haleem.waris@gmail.com>
2023-02-08 23:20:09 +0430

    Fix requests tests to match new specifications

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'PostsController', type: :request do
-  user = User.create(name: 'Waris',
-                     photo: 'photo.jpg', posts_counter: 5)
+RSpec.describe 'Posts controller Test', type: :request do
   subject do
-    Post.new(title: 'Something', text: 'A post from Waris!', comments_counter: 20, likes_counter: 16,
-             author: user)
-  end
+  @user = User.create(name: 'Waris', photo: 'photo.jpg',  bio: 'Developer', posts_counter: 3)
+  @post = Post.create(title: 'Ruby', text: 'A post from Waris about Ruby!', comments_counter: 20, likes_counter: 16, author: @user)
+end
 
   before { subject.save }
 
   describe 'GET #index' do
-    before(:each) { get user_posts_path(user_id: 1) }
+    before(:each) { get user_posts_path(user_id: @user.id) }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
@@ -19,9 +17,14 @@ RSpec.describe 'PostsController', type: :request do
     it 'renders the correct route' do
       expect(response).to render_template(:index)
     end
+
+    it 'has the correct placeholder text in the response body' do
+      expect(response.body).to include(@post.text)
+    end
   end
+
   describe 'GET #show' do
-    before(:each) { get user_post_path(user_id: 1, id: 1) }
+    before(:each) { get user_post_path(user_id: @user.id, id: @post.id) }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
@@ -31,7 +34,7 @@ RSpec.describe 'PostsController', type: :request do
     end
 
     it 'has the correct placeholder text in the response body' do
-      expect(response.body).to include('This page lists one post of a user!')
+      expect(response.body).to include(@post.text)
     end
   end
 end
