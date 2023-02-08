2ffeab28d3b24593e5aa7a67c897505797485b45
iAmWaris97 <haleem.waris@gmail.com>
2023-02-08 16:55:16 +0430

    Redirect to posts page after a comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Comment, type: :model do
 
   it 'testing update_comments_counter method' do
     author = User.create!(name: 'Waris',
-                          photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3QNOHWG_Ib38C-BaplM6TJx6VIxJcK0gYXaT1SuTJGw&s', bio: 'Web Developer', posts_counter: 0)
+                          photo: 'photo.jpg', bio: 'Web Developer', posts_counter: 0)
     post = Post.create!(author:, title: 'test post', text: 'First post from Waris', comments_counter: 0,
                         likes_counter: 0)
     Comment.create!(text: 'test comment', author:, post:)
