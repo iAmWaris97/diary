2b7229acf9e362291c122cbbcb54914b00d6ee57
iAmWaris97 <haleem.waris@gmail.com>
2023-02-11 16:14:14 +0430

    Able users to signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-   rescue_from CanCan::AccessDenied do | exception |
+  rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
+  end
+
+  def after_sign_out_path_for(scope)
+    p scope
+    new_user_session_path
   end
 
   protected
